755c4569735f831ae49db2671651f68d53746583
Student <example@example.com>
2020-01-14 07:30:02 -0800

    create CRUD for Project class

---
+++ lib/project.rb
@@ -0,0 +1,54 @@
+class Project
+  attr_reader :name
+  attr_reader :id
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+  end
+
+  def save
+    result = DB.exe("INSERT INTO projects (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
+
+  def update(name)
+    @name = name
+    DB.exec("UPDATE projects SET name ='#{@name}' WHERE id = #{@id};")
+  end
+
+  def ==(project_to_compare)
+    self.name == project_to_compare.name && self.id == project_to_compare.id
+  end
+
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each do |project|
+      name = project.fetch("name")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:name => name, :id => id}))
+    end
+    projects
+  end
+
+  def self.clear
+    DB.exec("DELETE FROM projects *;")
+  end
+
+  def self.find(id)
+    project = DB.exec("SELECT * FROM projects WHERE id = #{id};").first
+    name = project.fetch("name")
+    id = project.fetch("id").to_i
+    Project.new({:name =>, :id => id})
+  end
+
+  def delete
+    DB.exec("DELETE FROM projects WHERE id = #{@id};")
+    DB.exec("DELETE FROM volunteers WHERE project_id = #{@id}")
+  end
+
+  def volunteers
+    Volunteer.find_by_project(self.id)
+  end
+end
